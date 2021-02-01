df87416ab2bb3720beb64da4e09c1cfee3375379
Isaac Struhl <iostruhl@gmail.com>
2021-02-01 14:45:22 -0500

    updated vimsetup repo name

--- vimsetup/install-minimal.sh
+++ vimsetup/install-minimal.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -SsL https://raw.githubusercontent.com/iostruhl/vim-setup/master/install-minimal.sh | bash
+curl -SsL https://raw.githubusercontent.com/iostruhl/helper-scripts/master/install-minimal.sh | bash
 
